7ee91c61a7e3361d6907e93f76b4b161c0c4cc50
Nazir Knuckles <nazirknuckles@gmail.com>
2023-10-03 06:08:23 -0400

    Finnished coding quiz project!

--- Assets/script.js
+++ Assets/script.js
@@ -1,8 +1,8 @@
-const button = document.querySelector('button').addEventListener('click', startTimer);
 const mainContainer = document.querySelector('main');
 const counter = document.querySelector('#countDown');
 const completeContainer = document.querySelector('#complete');
 const messageContainer = document.querySelector('#message');
+const userInitials = document.getElementsByTagName('input');
 const h1 = document.querySelector('h1');
 const label = document.getElementsByTagName('label');
 const pElemnt = document.querySelectorAll('p');
@@ -11,6 +11,7 @@ const displayContent = 'block';
 mainContainer.children[4].style.display = noDisplay;
 completeContainer.style.display = noDisplay;
 let iterator = 0;
+const button = document.querySelector('button').addEventListener('click', displayNewPage);
 
 // Array of objects for each question
 const questions = [
@@ -56,21 +57,39 @@ const questions = [
     }
 ];
 
+// Displays a new page every time button is clicked
+function displayNewPage() {
+    if(!(iterator == 6) && !(iterator == 5)) startTimer();
+    else if(iterator == 5) {
+        showResults();
+        iterator++;
+    }
+    else location.reload();
+}
+
 // Starting the timer, and then starts the quiz
 function startTimer() {
     pElemnt[0].style.display = noDisplay;
     mainContainer.children[3].style.display = noDisplay;
     counter.textContent = 75;
-    setInterval(() => { if(!(parseInt(counter.textContent) <= 0)) counter.textContent -= 1; }, 1000);
+    setInterval(() => {
+        if(!(parseInt(counter.textContent) <= 0)) counter.textContent -= 1;
+        else {
+            if(iterator < 5) {
+                endQuiz();
+                iterator = 5;
+            }
+        }
+    }, 1000);
     mainContainer.classList.add('quizstart-styles');
     const btns = setButtons();
-    startQuiz(btns);
+    askAQuestion(btns);
 }
 
 // Prompts the quiz questions
-function startQuiz(btn) {
+function askAQuestion(btn) {
     for(let i = iterator; i == iterator; i++) {
-        if(iterator == questions.length) quizComplete();
+        if(iterator == questions.length) endQuiz();
         else {
             h1.textContent = questions[i].Question;
             btn[0].textContent = questions[i].Option1;
@@ -99,7 +118,11 @@ function setButtons() {
 
 // Listeners for users answer
 function setEventListeners(btns) {
-    for(let btn of btns) btn.addEventListener('click', (e) => validate(e, btns));
+    for(let btn of btns) {
+        btn.addEventListener('click', (e) => {
+            if(!(btns[1].textContent === 'Clear Highscores')) validate(e, btns);
+        })
+    }
 }
 
 // Validates if a user got the answer right or wrong, and deducts from time if user answered wrong.
@@ -132,24 +155,47 @@ function display(msg) {
 function isValid(usersAnswer, correctAnswer, btns) {
     if(usersAnswer.substring(3) === correctAnswer) {
         iterator++;
-        startQuiz(btns);
+        askAQuestion(btns);
         return true;
-    } else return false;
+    }
+    else return false;
 }
 
 // Quiz completed form
-function quizComplete() {
+function endQuiz() {
     for(let i = 4; i < mainContainer.children.length; i++) mainContainer.children[i].remove();
-    const va = counter.parentElement;
-    const vda = counter.parentElement.textContent;
-    va.textContent = vda
-    pElemnt[0].style.display = displayContent;
-    completeContainer.style.display = displayContent;
-    mainContainer.children[4].style.display = noDisplay;
-    mainContainer.children[3].style.display = 'inline';
+    const counterContainer = counter.parentElement;
+    console.log(counter)
+    const userScore = counter.parentElement.textContent;
+    counterContainer.textContent = userScore;
+    displayNewContent(4, noDisplay, 3, 'inline');
     mainContainer.children[3].textContent = 'Submit';
-    completeContainer.insertAdjacentElement('beforeend', mainContainer.children[3]);
+    pElemnt[0].style.display = displayContent;
     pElemnt[0].textContent = `Your final score is ${counter.textContent}`;
+    completeContainer.style.display = displayContent;
+    completeContainer.insertAdjacentElement('beforeend', mainContainer.children[3]);
     label[0].textContent = 'Enter Initials:';
     h1.textContent = 'All done!';
+}
+
+// Displays Quiz Results
+function showResults() {
+    h1.textContent = 'Highscores';
+    displayNewContent(4, displayContent, 3, displayContent);
+    mainContainer.children[2].children[2].textContent = 'Go Back';
+    mainContainer.children[3].textContent = 'Clear Highscores';
+    userInitials[0].style.display = displayContent;
+    userInitials[0].style.width = '100px';
+    userInitials[0].disabled = true;
+    userInitials[0].value = `1.${userInitials[0].value.toUpperCase()}-${document.body.children[0].children[1].textContent.substring(5)}`
+    label[0].remove();
+    pElemnt[0].remove();
+    mainContainer.children[2].addEventListener('click', () => {
+        if(userInitials[0] != undefined) userInitials[0].remove();
+    });
+}
+
+function displayNewContent(index1, value1, index2, value2) {
+    mainContainer.children[index1].style.display = value1;
+    mainContainer.children[index2].style.display = value2;
 }
