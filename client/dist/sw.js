6d27ff63b781495bf424e8e133760da603654f57
Nazir Knuckles <batking74@gmail.com>
2025-07-02 03:47:12 -0400

    Added dist

---
+++ client/dist/sw.js
@@ -0,0 +1,153 @@
+const staticCacheName = 'site-cache-v1';
+const dynamicCacheName = 'site-dynamic-v1';
+const assets = [
+    '/',
+    '/index.html',
+    '/manifest.json',
+    '/assets/Error.html'
+];
+
+// Install Event
+self.addEventListener('install', event => event.waitUntil(handleInstall()));
+
+// Activate Event
+self.addEventListener('activate', event => event.waitUntil(handleActivate()));
+
+// Fetch Event
+self.addEventListener('fetch', event => event.respondWith(handleFetch(event)));
+
+// Pre-Caching Assets
+async function handleInstall() {
+    try {
+        const cache = await caches.open(staticCacheName);
+        console.log('Caching assets during install');
+        await cache.addAll(assets);
+    }
+    catch (error) {
+        console.error('Failed to cache assets during install:', error);
+    }
+}
+
+
+async function handleActivate() {
+    try {
+        const cacheNames = await caches.keys();
+        await Promise.all(cacheNames.map(cacheName => cacheName !== staticCacheName && cacheName !== dynamicCacheName ? caches.delete(cacheName) : null));
+        console.log('Old caches cleaned up');
+    }
+    catch (error) {
+        console.error('Failed to clean up old caches:', error);
+    }
+}
+
+
+async function handleFetch(event) {
+    try {
+        // Check if the request is in the Api Cache
+        const cache = await caches.match(event.request);
+        if (cache) return cache;
+
+        // Check if the request is in IndexDB
+        const cachedResponseBlob = await getCachedResponse(event.request.url);
+        if (cachedResponseBlob) return new Response(cachedResponseBlob);
+
+        // Fetch from the Network
+        const response = await fetch(event.request);
+        const cacheStore = await caches.open(dynamicCacheName);
+
+        // Cache to both IndexDB and Api Cache
+        cacheStore.put(event.request, response.clone());
+        await cacheAPIResponse(event.request.url, await response.clone().blob());
+
+        // Limit the size of the cache
+        await limitCacheSize(cacheStore, 600);
+        return response;
+    }
+    catch (error) {
+        // Offline Fallback Page/Conditional Fallbacks
+        return caches.match('/Error.html');
+    }
+}
+
+async function limitCacheSize(cache, MAX_CACHE_ENTRIES) {
+    const keys = await cache.keys();
+    if (keys.length > MAX_CACHE_ENTRIES) {
+        // Delete the oldest entry
+        await cache.delete(keys[0]);
+    }
+}
+
+
+
+
+// Database
+const storeName = 'APIResponses';
+
+function openDatabase() {
+    return new Promise((resolve, reject) => {
+        const request = indexedDB.open('api-cache', 1);
+        request.onupgradeneeded = (event) => {
+            const db = event.target.result;
+            if (!db.objectStoreNames.contains(storeName)) db.createObjectStore(storeName, { keyPath: 'RequestURL' });
+        };
+        request.onsuccess = (event) => {
+            console.log('Api-Cache Database opened successfully!');
+            resolve(event.target.result);
+        }
+        request.onerror = (event) => {
+            console.error('Api-Cache Database error: ', event.target.errorCode);
+            reject(event.target.errorCode);
+        }
+    });
+}
+
+// Caches New APIResponse in indexDB
+function cacheAPIResponse(RequestURL, Response) {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const database = await openDatabase();
+            const transaction = await database.transaction([storeName], 'readwrite');
+            const objectStore = await transaction.objectStore(storeName);
+            const request = await objectStore.put({ RequestURL, Response });
+
+            request.onsuccess = () => resolve('APIResponse Cached Successfully');
+            request.onerror = (event) => {
+                console.error('Error Caching APIResponse to indexDB:', event.target.errorCode);
+                reject(event.target.errorCode);
+            }
+        }
+        catch (error) {
+            console.error('Error in cacheAPIResponse Function:', error);
+            throw error;
+        }
+    });
+}
+
+// Retrieves an APIResponse if exists in indexDB
+function getCachedResponse(requestURL) {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const database = await openDatabase();
+            const transaction = database.transaction([storeName], 'readonly');
+            const objectStore = transaction.objectStore(storeName);
+            const request = objectStore.get(requestURL);
+
+            request.onsuccess = () => {
+                if (request.result) {
+                    console.log('APIResponse Retrieved:', request.result);
+                    resolve(request.result.Response);
+                }
+                else resolve(null);
+                resolve(request.result);
+            }
+            request.onerror = (event) => {
+                console.error('Error Retrieving APIResponse:', event.target.errorCode);
+                reject(event.target.errorCode);
+            }
+        }
+        catch (error) {
+            console.error('Error in Opening Database in getCachedResponse() Function:', error);
+            throw error;
+        }
+    });
+}
